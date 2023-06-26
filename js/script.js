904a7a8f048d434ef598e50900787ff7fd569cdd
TiemiMorishita <tiemimorishita12@gmail.com>
2023-06-26 00:13:37 -0300

    1

---
+++ js/script.js
@@ -0,0 +1,47 @@
+d1 = 10;
+d2 = 5;
+
+$("#anima-div").css({"left" : d2+"vw", "top" : d1+"vh"})
+
+
+$("#btn-cima").click(() =>{
+        d1 -= 10 
+        $("#anima-div").animate({ top: d1 + "vh" }, { duration: 200 })
+        $("#anima-div").removeClass("anima-div-3")
+        $("#anima-div").addClass("anima-div")
+    }
+)
+
+$("#btn-baixo").click(() =>{
+        d1 += 10  
+        $("#anima-div").animate({ top: d1 + "vh" }, { duration: 200 })
+        $("#anima-div").removeClass("anima-div-3")
+        $("#anima-div").addClass("anima-div")
+    }
+)
+
+$("#btn-esquerda").click(() =>{
+        d2 -= 5
+        $("#anima-div").animate({ left:d2 +"vw" }, { duration: 200 })
+        $("#anima-div").removeClass("anima-div-3")
+        $("#anima-div").removeClass("anima-div")
+        $("#anima-div").addClass("anima-div-2")
+    }
+)
+
+$("#btn-direita").click(() =>{
+        d2 += 5
+        $("#anima-div").animate({ left: d2 + "vw" }, { duration: 200 } )
+        $("#anima-div").removeClass("anima-div-3")
+        $("#anima-div").removeClass("anima-div-2")
+        $("#anima-div").addClass("anima-div")
+    }
+)
+
+$("#btn-meio").click(() => {
+    $("#anima-div").removeClass("anima-div")
+    $("#anima-div").addClass("anima-div-3")
+})
+
+
+
